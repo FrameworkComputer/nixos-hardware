c45bb6f0d8907ad679e549bbd369235699b0b491
Daniel Schaefer <git@danielschaefer.me>
2025-07-24 12:58:11 +0800

    framework/12-inch: Fix tabletmode on some kernels
    NixOS kernels don't have pinctrl_tigerlake built-in, we need to make
    sure it loads before soc_button_array. Adding it to the initrd ensures
    that.
    
    Signed-off-by: Daniel Schaefer <git@danielschaefer.me>

--- framework/12-inch/common/default.nix
+++ framework/12-inch/common/default.nix
@@ -8,6 +8,12 @@
     ../../framework-tool.nix
   ];
 
+  # If this module isn't built into the kernel, we need to make sure it loads
+  # before soc_button_array. Otherwise the tablet mode gpio doesn't work.
+  # If correctly loaded, dmesg should show
+  # input: gpio-keys as /devices/platform/INT33D3:00
+  boot.initrd.kernelModules = [ "pinctrl_tigerlake" ];
+
   # Fix TRRS headphones missing a mic
   # https://github.com/torvalds/linux/commit/7b509910b3ad6d7aacead24c8744de10daf8715d
   boot.extraModprobeConfig = lib.mkIf (lib.versionOlder config.boot.kernelPackages.kernel.version "6.13.0") ''
